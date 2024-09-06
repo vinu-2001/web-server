f0965ff80a91fadf6ec3684d7cb7581935ae3604
vinu <suryawanshivinayak54@gmail.com>
2024-09-06 14:49:55 +0530

    Commented unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side javascript file is loaded1')
+// console.log('Client side javascript file is loaded1')
 
 // fetch('http://puzzle.mead.io/puzzle').then((response)=>{
 //     response.json().then((data)=>{
